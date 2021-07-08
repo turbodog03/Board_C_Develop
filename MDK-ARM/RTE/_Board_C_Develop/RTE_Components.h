0e35c8750f52764bd89751620c12dfdf8f8e8d77
turbodog03 <63595854+turbodog03@users.noreply.github.com>
2021-07-08 11:31:09 +0800

    fix git ignore

---
+++ MDK-ARM/RTE/_Board_C_Develop/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Board_C_Develop' 
+ * Target:  'Board_C_Develop' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
